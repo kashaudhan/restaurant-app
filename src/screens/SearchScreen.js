0c9f924b5fee25870a2e23ffd90aeff18c8e2807
kashaudhan <kashaudhan94@gmail.com>
2022-01-11 00:18:05 +0530

    app setup

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function SearchScreen() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+})
